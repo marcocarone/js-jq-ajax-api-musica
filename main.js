2bc46bd910b9b46dffd679bc7c204921286584f4
Marco <marco.carone@libero.it>
2020-01-31 16:02:09 +0100

    fine esercizio

---
+++ main.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+  tuttiGeneri()
+
+  $(document).on("mouseout", "select", function() {
+    var questo = $(this);
+    console.log(questo.val());
+    if (questo.val() == "Pop") {
+      risultato("Pop");
+      $(".cd").remove();
+    } else if (questo.val() == "Rock") {
+      risultato("Rock");
+      $(".cd").remove();
+
+    } else if (questo.val() == "Metal") {
+      risultato("Metal");
+      $(".cd").remove();
+    } else if (questo.val() == "Jazz") {
+      risultato("Jazz");
+      $(".cd").remove();
+    } else if (questo.val() == "Tutti") {
+      tuttiGeneri()
+      $(".cd").remove();
+    }
+  });
+
+});
+
+
+function risultato(genere) {
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(data, stato) {
+      // console.log(data.response);
+      var cds = data.response;
+      for (var i = 0; i < cds.length; i++) {
+        var cd = cds[i];
+        // console.log(cd);
+        if (cd.genre == genere) {
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          var html = template(cd);
+          $(".cds-container").append(html);
+        }
+      }
+    },
+    error: function(richiesta, stato, errore) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+}
+
+
+function tuttiGeneri() {
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function(data, stato) {
+      // console.log(data.response);
+      var cds = data.response;
+      for (var i = 0; i < cds.length; i++) {
+        var cd = cds[i];
+        // console.log(cd);
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(cd);
+        $(".cds-container").append(html);
+      }
+    },
+    error: function(richiesta, stato, errore) {
+      alert("E' avvenuto un errore. " + errore);
+    }
+  });
+}
